e2216ad79e8173de31997e21668113f8d75b5e6c
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-06-26 20:14:57 -0300

    chore: add eslint config setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,27 @@
-import { defineConfig } from "eslint/config";
-import tseslint from "typescript-eslint";
-import globals from "globals";
-import json from "@eslint/json";
+import { defineConfig } from '@typescript-eslint/config'
+import globals from 'globals'
 
-export default defineConfig([
-  {
-    ignores: ["**/*.js", "**/*.cjs", "**/*.mjs"],
-  },
-
-  {
-    files: ["**/*.json"],
-    plugins: { json },
-    languageOptions: { parser: json.parser },
-    rules: json.configs.recommended.rules,
-  },
-
-  tseslint.configs.recommended,
-  {
-    files: ["**/*.ts"],
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        project: "./tsconfig.json",
-      },
-      sourceType: "module",
-      ecmaVersion: "latest",
-      globals: {
-        ...globals.node,
-        ...globals.browser,
-        ...globals.jest,
-      },
+export default defineConfig({
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: './tsconfig.json',
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    env: {
+        node: true,
+        jest: true,
     },
     rules: {
-      "@typescript-eslint/explicit-function-return-type": "off",
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        { argsIgnorePattern: "^_" },
-      ],
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            { argsIgnorePattern: '^_' },
+        ],
     },
-  },
-]);
+})
