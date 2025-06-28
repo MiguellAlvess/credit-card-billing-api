f6a26aa455d26446c79dad7ff1e240e7c15f3eb9
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-06-28 20:27:08 -0300

    chore: disable @typescript-eslint/no-explicit-any rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,6 +22,7 @@ export default [
     {
         rules: {
             '@typescript-eslint/explicit-function-return-type': 'off',
+            '@typescript-eslint/no-explicit-any': 'off',
             '@typescript-eslint/no-unused-vars': [
                 'warn',
                 { argsIgnorePattern: '^_' },
