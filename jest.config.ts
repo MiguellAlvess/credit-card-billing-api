742e475ae71a7081e58813dd0a6a816c0f2a2fb8
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-06-28 16:00:57 -0300

    chore: ensure jest has correct support for typescript

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,11 @@
 import type { Config } from 'jest'
 
 const config: Config = {
-    clearMocks: true,
-    collectCoverage: true,
-    coverageDirectory: 'coverage',
-    coverageProvider: 'v8',
+    preset: 'ts-jest',
     testEnvironment: 'node',
+    clearMocks: true,
     collectCoverageFrom: ['src/**/*.ts'],
+    testMatch: ['**/test/**/*.test.ts'],
 }
 
 export default config
