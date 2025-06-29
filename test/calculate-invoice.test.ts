dc27b5ef0801ca1616473ef9928eb60550161640
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-06-28 22:17:20 -0300

    test: ensure CalculateInvoiceUseCase calculate invoices correctly

--- test/calculate-invoice.test.ts
+++ test/calculate-invoice.test.ts
@@ -1,9 +1,35 @@
 import CalculateInvoiceUseCase from '../src/layers/application/calculate-invoice'
+import CurrencyGateway from '../src/layers/resources/ports/gateway/currency-gateway'
+import { GetTransactionsDAO } from '../src/layers/resources/ports/daos/get-transactions-dao'
 
 describe('Calculate Invoice Use Case', () => {
     test('should calculate invoice', async () => {
-        const sut = new CalculateInvoiceUseCase()
+        const getTransactionsDAO: GetTransactionsDAO = {
+            async execute(
+                cardNumber: string,
+                month: number,
+                year: number,
+            ): Promise<any> {
+                return [
+                    { amount: 100, currency: 'BRL' },
+                    { amount: 1000, currency: 'BRL' },
+                    { amount: 600, currency: 'USD' },
+                ]
+            },
+        }
+
+        const currencyGateway: CurrencyGateway = {
+            async getCurrencies(): Promise<any> {
+                return { usd: 2 }
+            },
+        }
+
+        const sut = new CalculateInvoiceUseCase(
+            getTransactionsDAO,
+            currencyGateway,
+        )
+
         const total = await sut.execute('1234')
-        expect(total).toBe(1050)
+        expect(total).toBe(2300)
     })
 })
