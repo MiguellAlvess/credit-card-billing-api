a39e7843c4c9c7f9867fd7edfea11f5935b3ef73
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-06-28 22:16:27 -0300

    refactor: decouple external prisma with DatabaseClient interface

--- src/layers/resources/adapters/daos/get-transactions-dao-database.ts
+++ src/layers/resources/adapters/daos/get-transactions-dao-database.ts
@@ -1,27 +1,25 @@
-import prisma from '../../../../../prisma/prisma'
-import { Prisma } from '@prisma/client'
+import { DatabaseClient } from '../../../infrastructure/database/database-client'
 import { GetTransactionsDAO } from '../../ports/daos/get-transactions-dao'
 
 export default class GetTransactionsDAODatabase implements GetTransactionsDAO {
+    constructor(readonly databaseClient: DatabaseClient) {}
+
     async execute(
         cardNumber: string,
         month: number,
         year: number,
-    ): Promise<
-        Array<{
-            amount: Prisma.Decimal
-            currency: string
-        }>
-    > {
-        const transactions = await prisma.cardTransaction.findMany({
-            where: {
-                card_number: cardNumber,
-                date: {
-                    gte: new Date(year, month - 1, 1),
-                    lt: new Date(year, month, 1),
+    ): Promise<Array<any>> {
+        const transactions = await this.databaseClient.cardTransaction.findMany(
+            {
+                where: {
+                    card_number: cardNumber,
+                    date: {
+                        gte: new Date(year, month - 1, 1),
+                        lt: new Date(year, month, 1),
+                    },
                 },
             },
-        })
+        )
 
         return transactions
     }
