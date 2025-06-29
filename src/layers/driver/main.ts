a4447957a55db4ac1e9aa965e5ed2ec5b45e9e7f
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-06-28 22:19:20 -0300

    refactor: add infrastructure adapters and improve dependency injection in main

--- src/layers/driver/main.ts
+++ src/layers/driver/main.ts
@@ -3,12 +3,18 @@ import 'dotenv/config'
 import CalculateInvoiceUseCase from '../application/calculate-invoice'
 import GetTransactionsDAODatabase from '../resources/adapters/daos/get-transactions-dao-database'
 import CurrencyGatewayHttp from '../resources/adapters/gateway/currency-gateway-http'
+import AxiosAdapter from '../infrastructure/http/axios-adapter'
+
+import { PrismaAdapter } from '../infrastructure/database/prisma-adapter'
 
 const app = express()
 
 app.get('/cards/:cardNumber/invoices', async (req, res) => {
-    const getTransactionsDAO = new GetTransactionsDAODatabase()
-    const currencyGateway = new CurrencyGatewayHttp()
+    const databaseClient = new PrismaAdapter()
+    const getTransactionsDAO = new GetTransactionsDAODatabase(databaseClient)
+    const httpClient = new AxiosAdapter()
+    const baseUrl = 'http://localhost:3001'
+    const currencyGateway = new CurrencyGatewayHttp(httpClient, baseUrl)
     const calculteInvoice = new CalculateInvoiceUseCase(
         getTransactionsDAO,
         currencyGateway,
